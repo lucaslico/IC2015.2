6cfecc9ffdbd479e003b65e98936e49b9f8e3e90
Lucas Lima <lucaslico3179@gmail.com>
2016-04-27 16:31:23 -0300

    Update taxaerros.py

--- taxaerros.py
+++ taxaerros.py
@@ -1,13 +1,37 @@
-import os
-import enchant
-
-pt = enchant.Dict('pt_BR')
-en = enchant.Dict('en_US')
-
+def mv(): #move os arquivos para a pasta
+    onlyfiles2 = glob.glob('*.txt')
+    for i in range(0,len(onlyfiles2)):
+        a = onlyfiles2[i]
+        command_line = "mv {} txt_py".format(a)
+        args = shlex.split(command_line)
+        subprocess.Popen(args)
+mv()
+def dim_erros(): #diminui erros 
+    onlyfiles = [f for f in os.listdir(os.path.join(os.getcwd(),'txt_py')) if os.path.isfile(os.path.join(os.path.join(os.getcwd(),'txt_py'), f))]
+    contador = len(onlyfiles)
+    b = os.path.join(os.getcwd(),'txt_py') + '/'
+    for i in range(0,len(onlyfiles)):
+        onlyfiles[i] = b + onlyfiles[i]
+    list.sort(onlyfiles)
+    n = contador
+    while contador > 0:
+        txt = onlyfiles[n-contador]
+        contador -= 1
+        arquivo = open(txt, 'r',5)
+        string = "".join(str(arquivo.read()))
+        string = string.replace('-\n','')
+        arquivo2 = open(txt,'w',5)
+        arquivo2.write(string)
+        arquivo2.close()
+dim_erros()
 def taxa(): #programa que devolve a taxa de erro
     erro = []
     total = []
-    onlyfiles = [f for f in os.listdir('/home/lucas/FGV/IC/txt py/') if os.path.isfile(os.path.join('/home/lucas/FGV/IC/txt py/', f))] #varia de acordo com a pasta
+    b = os.path.join(os.getcwd(),'txt_py') + '/'
+    onlyfiles = [f for f in os.listdir(os.path.join(os.getcwd(),'txt_py')) if os.path.isfile(os.path.join(os.path.join(os.getcwd(),'txt_py'), f))]
+    for i in range(0,len(onlyfiles)):
+        onlyfiles[i] = b + onlyfiles[i]
+    list.sort(onlyfiles)
     contador = len(onlyfiles)
     n = contador
     while contador > 0:
@@ -26,3 +50,4 @@ def taxa(): #programa que devolve a taxa de erro
     e = sum(erro)
     t = sum(total)
     print float(e)/float(t)
+taxa()
